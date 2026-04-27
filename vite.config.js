e8f6a8c07bfb24f831260407ed4c234e982e5a39
elly <ellysabet81@gmail.com>
2026-04-27 20:06:15 +0900

    fix: 배포 경로 설정 및 메뉴 명칭 최종 통일

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wemake_homepage/', // 반드시 저장소 이름 앞뒤로 /를 붙여주세요!
 })
