138afbc288973742a5013bfed2e89b0a31da934a
ellysabet <147838098+ellysabet@users.noreply.github.com>
2026-05-31 10:56:44 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wemake_homepage/', // GitHub Pages 배포를 위한 정확한 경로 설정
+  base: '/', // GitHub Pages 배포를 위한 정확한 경로 설정
 })
